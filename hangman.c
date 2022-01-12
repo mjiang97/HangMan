2e276321c54151a57231e42d53ed64c737d29a55
Hebe3834 <hebeluna00@gmail.com>
2022-01-12 10:59:52 -0500

    oops

---
+++ hangman.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <ncurses.h>
+
+int main(){
+
+  initscr();			/* Start curses mode 		  */
+	printw("Hello World !!!");	/* Print Hello World		  */
+	refresh();			/* Print it on to the real screen */
+	getch();			/* Wait for user input */
+	endwin();			/* End curses mode		  */
+
+
+
+  return 0;
+}
